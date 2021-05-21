b7e1be90558d14dce1b5202713ca7c3ed1c6c303
Creepsy <Robin.gd@outlook.de>
2021-05-21 21:26:18 +0200

    added basic functions for parser

--- src/regex/regex_parser.cpp
+++ src/regex/regex_parser.cpp
@@ -1,9 +1,37 @@
 #include "regex_parser.h"
 
+#include <stdexcept>
+
 using namespace regex;
 
 regex_parser::regex_parser(regex_lexer& lexer) : lexer(lexer) {
+    this->next();
 }
 
 regex_parser::~regex_parser() {
+}
+
+// private
+
+void regex_parser::next() {
+    this->curr = this->lexer.next_token();
+}
+
+bool regex_parser::accept(const std::vector<token::token_type>& types) {
+    for(const token::token_type type : types) {
+        if(this->accept(type)) return true;
+    }
+    return false;
+}
+
+bool regex_parser::accept(const token::token_type type) {
+    return this->curr.type == type;
+}
+
+void regex_parser::expect(const token::token_type type) {
+    if(!this->accept(type)) throw std::runtime_error("Invalid token!");
+}
+
+void regex_parser::expect(const std::vector<token::token_type>& types) {
+    if(!this->accept(types)) throw std::runtime_error("Invalid token!");
 }
--- src/regex/regex_parser.h
+++ src/regex/regex_parser.h
@@ -7,6 +7,13 @@ namespace regex {
     class regex_parser {
         private:
             regex_lexer& lexer;
+            token curr;
+
+            void next();
+            bool accept(const std::vector<token::token_type>& types);
+            bool accept(const token::token_type type);
+            void expect(const token::token_type type);
+            void expect(const std::vector<token::token_type>& types);
         public:
             regex_parser(regex_lexer& lexer);
             ~regex_parser();
